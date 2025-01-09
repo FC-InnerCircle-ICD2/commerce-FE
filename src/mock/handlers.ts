be322e54c24dfd30f2f3f30f26790768b6580c7c
김지훈 <kigpand@gmail.com>
2025-01-09 22:44:28 +0900

    feat: msw 설정 파일 추가

---
+++ src/mock/handlers.ts
@@ -0,0 +1,22 @@
+// src/mocks/handlers.js
+// mocking api handler
+import { http, HttpResponse } from 'msw';
+
+// mocking할 api 설정
+export const handlers = [
+  // Intercept "GET https://example.com/user" requests...
+  http.get('https://example.com/user', () => {
+    // ...and respond to them using this JSON response.
+    return HttpResponse.json({
+      id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
+      firstName: 'John',
+      lastName: 'Maverick',
+    });
+  }),
+
+  http.get('http://example.com/client', () => {
+    return HttpResponse.json({
+      test: 'success',
+    });
+  }),
+];
