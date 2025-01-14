42c55873c9f9896b0593d93b3388474cd0ae2d89
김지훈 <kigpand@gmail.com>
2025-01-14 21:10:31 +0900

    feat: 모바일전용 footer layout 생성,

--- src/components/layout/header/CategoryForm.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-type Props = {
-  handleCloseForm: () => void;
-};
-
-export default function CategoryForm({ handleCloseForm }: Props) {
-  return <div onClick={handleCloseForm}>close</div>;
-}
