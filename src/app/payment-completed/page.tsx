73eb8847562f8f23c83f7a279a4aebc763cb40ca
SeokHoChoi <seokho9473@gmail.com>
2025-01-15 11:30:42 +0900

    feat: 구현 내용 대분류

--- src/app/payment-completed/page.tsx
+++ src/app/payment-completed/page.tsx
@@ -1,8 +1,17 @@
 export default function PaymentCompleted() {
   return (
     <div className="">
+      {/* TODO: 상단 네비 */}
+      <div>주문/결제 {`>`} 완료</div>
+
+      {/* TODO: 배경 컨테이너 적용 */}
       <div>
-        <h1>Payment Completed</h1>
+
+        {/* TODO: 주문 완료 정보 */}
+        <div></div>
+
+        {/* TODO: 추천 상품 */}
+        <div></div>
       </div>
     </div>
   );
