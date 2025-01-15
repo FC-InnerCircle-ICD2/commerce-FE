11fb944a845c6d24becd3c2d5069fbe6bf41ebc3
SeokHoChoi <seokho9473@gmail.com>
2025-01-15 11:37:05 +0900

    feat: 구현 내용 소분류

--- src/app/payment-completed/page.tsx
+++ src/app/payment-completed/page.tsx
@@ -8,10 +8,29 @@ export default function PaymentCompleted() {
       <div>
 
         {/* TODO: 주문 완료 정보 */}
-        <div></div>
+        <div>
+          <div>
+            <span>주문완료 </span> 
+            <span>되었습니다.</span>
+          </div>
+          <div>
+            <div />
+            <span>주문번호 </span>
+            <span>253..</span>
+            <div />
+            <span>CSH(집)</span>
+            <span>010-1000-2000</span>
+            <span>서울 특별시 -- 101동 1004호</span>
+            <div />
+          </div>
+          <button>홈으로</button>
+        </div>
 
         {/* TODO: 추천 상품 */}
-        <div></div>
+        <div>
+          <div>이런 상품 어때요?</div>
+          <div>캐러셀</div>
+        </div>
       </div>
     </div>
   );
