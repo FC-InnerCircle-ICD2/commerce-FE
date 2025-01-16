f0c30253b83581f991f1e8713d8275c15bf806bf
김지훈 <kigpand@gmail.com>
2025-01-16 22:42:03 +0900

    feat: 공용 검색 input 컴포넌트 개발

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,4 @@ export { default as Card } from './Card';
 export { default as CategorySearch } from './CategorySearch';
 export { default as Selectbox } from './Selectbox';
 export { default as SearchForm } from './searchForm/SearchForm';
+export { default as SearchInput } from './SearchInput';
