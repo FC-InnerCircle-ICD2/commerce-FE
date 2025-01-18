08997cbf1b1c6a0694e725f7c3b27ab4a50ebef9
김지훈 <kigpand@gmail.com>
2025-01-18 17:41:50 +0900

    feat: product api 연동

---
+++ src/api/product.ts
@@ -0,0 +1,83 @@
+import { buildUrl } from '@/utils/buildUrl';
+import type { ICategory } from './category';
+
+export interface IProduct {
+  productId: number;
+  name: string;
+  description: string;
+  category: {
+    categoryId: number;
+    name: string;
+    parentCategoryId: number;
+    subCategories: ICategory[];
+  };
+  provider: {
+    providerId: number;
+    name: string;
+    description: string;
+  };
+  options: [
+    {
+      id: number;
+      name: string;
+      value: string;
+      quantity: number;
+      optionOrder: number;
+      sellPrice: number;
+    },
+  ];
+  images: [
+    {
+      id: number;
+      url: string;
+      fileOrder: number;
+      isRepresentative: boolean;
+    },
+  ];
+}
+
+interface IProductAPI {
+  contents: IProduct[];
+  //   TODO: page interface 만들어논거로 교체 예정
+  page: {
+    size: number;
+    number: number;
+    totalElements: number;
+    totalPages: number;
+  };
+}
+
+const PRODUCT_URL = '/api/v1/products';
+
+type ProductsProps = {
+  /** 조회할 카테고리 ID */
+  productCategoryId?: number;
+  /** 검색할 제품 이름 */
+  name?: string;
+  /** 별점  */
+  rating?: number;
+  /** 정렬기준
+   * registration=등록순, sales=판매량순, priceAsc=낮은가격순, priceDesc=높은가격순
+   */
+  sort?: 'registration' | 'sales' | 'priceAsc' | 'priceDesc';
+  page?: number;
+  size?: number;
+};
+
+export const getProducts = async ({ productCategoryId, name, rating, sort }: ProductsProps): Promise<IProduct[]> => {
+  const url = buildUrl(`https://virtserver.swaggerhub.com/SMJ9030_1/Product-MVP/1.0.0${PRODUCT_URL}`, {
+    productCategoryId,
+    sort,
+    rating,
+    name,
+  });
+
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`Error fetching user: ${response.statusText}`);
+  }
+
+  const data: IProductAPI = await response.json();
+  return data.contents;
+};
---
+++ src/utils/buildUrl.ts
@@ -0,0 +1,12 @@
+export function buildUrl(base: string, params: Record<string, string | number | undefined>): string {
+  const searchParams = new URLSearchParams();
+
+  // 조건부로 파라미터 추가
+  for (const [key, value] of Object.entries(params)) {
+    if (value !== undefined) {
+      searchParams.append(key, String(value));
+    }
+  }
+
+  return `${base}?${searchParams.toString()}`;
+}
