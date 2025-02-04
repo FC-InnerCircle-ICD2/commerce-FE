1e3960bf3f992c03022ed7a8e1d02b76aa7249ce
김지훈 <kigpand@gmail.com>
2025-02-04 22:39:38 +0900

    feat: 모바일 카테고리 페이지 카테고리 헤더 화살표 클릭시 홈화면이 아니라 뒤로가도록 처리

--- src/app/category/_components/CategoryHeader.tsx
+++ src/app/category/_components/CategoryHeader.tsx
@@ -1,13 +1,11 @@
 'use client';
 
 import Image from 'next/image';
-import Link from 'next/link';
-import { ShoppingCartIcon } from '@heroicons/react/24/outline';
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { useCategory } from '@/hooks/queries/useCategory';
 
 export default function CategoryHeader() {
+  const router = useRouter();
   const searchParams = useSearchParams();
   const categoryId = searchParams.get('category');
   const keyword = searchParams.get('name');
@@ -44,18 +42,10 @@ export default function CategoryHeader() {
   return (
     <header className="w-full h-[60] flex justify-between items-center px-4 shadow-md">
       <div className="flex gap-5">
-        <Link href="/">
+        <div onClick={() => router.back()}>
           <Image src="/assets/arrow.svg" width={24} height={24} alt="arrow" />
-        </Link>
+        </div>
         <span className="font-bold">{getTitle()}</span>
-      </div>
-      <div className="flex gap-[15]">
-        <div className="w-10 h-10 rounded-lg flex items-center justify-center bg-[#F6F8FB]">
-          <MagnifyingGlassIcon className="w-[30px] h-[30px] text-[#075985]" />
-        </div>
-        <div className="w-10 h-10 rounded-lg flex items-center justify-center bg-[#F6F8FB]">
-          <ShoppingCartIcon className="w-[30px] h-[30px] text-[#075985]" />
-        </div>
       </div>
     </header>
   );
