27bc75373672e746c4f7cf5d42c2e60bc56bada1
김지훈 <kigpand@gmail.com>
2025-02-06 20:50:01 +0900

    feat: banner 임시 아이콘 등록 및 배너 클릭시 해당 상품으로 이동하도록 적용

--- src/components/home/Carousel.tsx
+++ src/components/home/Carousel.tsx
@@ -2,12 +2,14 @@
 
 import type { IBanner } from '@/api/banner';
 import { BannerCarousel } from 'ji-react-carousel';
+import { useRouter } from 'next/navigation';
 
 type Props = {
   banners: IBanner[];
 };
 
 export default function Carousel({ banners }: Props) {
+  const router = useRouter();
   return (
     <div className="w-full h-[350] bg-slate-200">
       <BannerCarousel
@@ -15,12 +17,20 @@ export default function Carousel({ banners }: Props) {
         autoTimer={2000}
         bannerInfo={[
           ...banners.map((item) => {
-            return { iconUrl: item.iconUrl, title: item.title };
+            return { iconUrl: '/assets/paw.svg', title: item.title };
           }),
         ]}
       >
         {banners.map((item) => {
-          return <img key={item.id} src={item.bannerImage.url} alt={item.title} className="w-full h-full" />;
+          return (
+            <img
+              key={item.id}
+              src={item.bannerImage.url}
+              alt={item.title}
+              className="w-full h-full cursor-pointer"
+              onClick={() => router.push(item.productBannerResponse.linkUrl)}
+            />
+          );
         })}
       </BannerCarousel>
     </div>
