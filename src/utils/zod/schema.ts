0ede617b3e31ff858ac8aac80f85464a30aa3722
김지훈 <kigpand@gmail.com>
2025-02-14 00:04:16 +0900

    feat: zod 스키마 추가

--- src/utils/zod/schema.ts
+++ src/utils/zod/schema.ts
@@ -14,3 +14,22 @@ export const idSchema = z.object({
     .regex(/[0-9]/, '비밀번호에 최소 하나의 숫자가 포함되어야 합니다.')
     .regex(/[\W_]/, '비밀번호에 최소 하나의 특수문자가 포함되어야 합니다.'),
 });
+
+export const emailSchema = z.object({
+  email: z
+    .string()
+    .min(5, { message: '이메일은 최소 5자 이상이어야 합니다.' })
+    .max(50, { message: '이메일은 최대 50자 이하로 입력해야 합니다.' })
+    .email({ message: '올바른 이메일 형식이 아닙니다.' }),
+});
+
+export const textSchema = z.object({
+  text: z
+    .string()
+    .min(2, { message: '텍스트는 최소 2자 이상이어야 합니다.' })
+    .regex(/^[A-Za-z0-9가-힣]+$/, { message: '특수문자는 사용할 수 없습니다.' }),
+});
+
+export const numberSchema = z.object({
+  number: z.string().regex(/^[0-9]+$/, { message: '숫자만 입력 가능합니다.' }),
+});
