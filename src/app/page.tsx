886cf9aede2f4b4cc5bdd49b1fb59a403f34bbd8
최석호 <seokho.choi@ikoob.com>
2025-02-25 00:56:51 +0900

    feat: 로그인 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,23 @@ import Carousel from '@/components/home/Carousel';
 import ProductList from '@/components/home/ProductList';
 import { ProductSkeleton } from '@/components/skeletons';
 import { headers } from 'next/headers';
-
 import { Header } from '@/components/layout';
 import { Suspense } from 'react';
+import { TokenHandler } from '@/components/TokenHandler';
 
 export default async function Home() {
   try {
     const banners = await getBanners();
     const headerList = await headers();
-    const cookieHeader = headerList.get('cookie'); // 전체 쿠키 문자열을 가져옴
-    console.log(headerList);
-    console.log(cookieHeader);
-
+    const cookieHeader = headerList.get('cookie') || '';
+    
+    // 쿠키에서 accessToken 추출
+    const accessToken = cookieHeader.split('; ').find(row => row.startsWith('Access-Token='))?.split('=')[1] || null;
+  
     return (
       <div className="flex flex-col h-screen overflow-x-hidden">
         <Header />
+        <TokenHandler accessToken={accessToken} /> {/* 새로운 컴포넌트 추가 */}
         <div className="grow flex flex-col gap-5">
           <Carousel banners={banners} />
           <Suspense fallback={<ProductSkeleton />}>
