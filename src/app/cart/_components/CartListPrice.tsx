2f0def709322a01979bb5680c43332a99d31816d
김지훈 <kigpand@gmail.com>
2025-02-27 23:56:03 +0900

    feat: 장바구니 조회 업데이트되도록 처리, numberFormatting 적용

--- src/app/cart/_components/CartListPrice.tsx
+++ src/app/cart/_components/CartListPrice.tsx
@@ -1,4 +1,5 @@
 import { ICartItem } from '@/api/cart';
+import { numberFormatting } from '@/utils/numberFormatting';
 
 type Props = {
   product: ICartItem;
@@ -9,7 +10,7 @@ export default function CartListPrice({ product }: Props) {
     <div className="grow flex items-end gap-[20px] justify-between flex-col p-[20px] border-l tablet:h-full tablet:items-center tablet:border-l-0">
       <div className="flex gap-[3px] w-full justify-between tablet:flex-col tablet:items-center">
         <h1 className="text-xs font-bold">상품금액</h1>
-        <p className="text-sm font-bold">{product.price}원</p>
+        <p className="text-sm font-bold">{numberFormatting(product.price)}원</p>
       </div>
       <button className="text-xs border border-blue-300 py-[4px] px-[8px] rounded-sm font-semibold text-blue-500">
         주문하기
