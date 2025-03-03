a7e5505eb0dcee9b045b8861d359320d83fab126
김지훈 <kigpand@gmail.com>
2025-03-03 23:43:52 +0900

    feat: 리뷰 이미지 멀티 등록 추가

--- src/app/review/_components/ReviewForm.tsx
+++ src/app/review/_components/ReviewForm.tsx
@@ -15,8 +15,8 @@ type Props = {
 };
 
 export default function ReviewForm({ orderId, product, productOptionId, productOptionName }: Props) {
-  const [file, setFile] = useState<File | null>(null);
-  const [preview, setPreview] = useState<string | null>(null);
+  const [files, setFiles] = useState<File[]>([]);
+  const [previews, setPreviews] = useState<string[]>([]);
   const [content, setContent] = useState<string>('');
   const [rating, setRating] = useState<string>('0');
   const fileInputRef = useRef<HTMLInputElement | null>(null);
@@ -30,10 +30,14 @@ export default function ReviewForm({ orderId, product, productOptionId, productO
 
   // 파일 선택 핸들러
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedFile = event.target.files?.[0];
-    if (selectedFile) {
-      setFile(selectedFile);
-      setPreview(URL.createObjectURL(selectedFile)); // 이미지 미리보기 설정
+    const selectedFiles = event.target.files;
+    if (selectedFiles) {
+      const fileArray = Array.from(selectedFiles); // FileList -> 배열 변환
+      setFiles((prevFiles) => [...prevFiles, ...fileArray]);
+
+      // 이미지 미리보기 생성
+      const previewArray = fileArray.map((file) => URL.createObjectURL(file));
+      setPreviews((prevPreviews) => [...prevPreviews, ...previewArray]);
     }
   };
 
@@ -48,7 +52,7 @@ export default function ReviewForm({ orderId, product, productOptionId, productO
   };
 
   const handleUpload = async () => {
-    if (!file || !content || !rating) {
+    if (!files || !content || !rating) {
       alert('빈값이 존재하면 안됩니다');
       return;
     }
@@ -57,12 +61,15 @@ export default function ReviewForm({ orderId, product, productOptionId, productO
 
     formData.append('orderId', orderId);
     formData.append('productName', product?.name ?? '');
-    // TODO: 해당 부분들 배열형식으로 수정되야할듯.
+    // TODO: 해당 부분 삭제 논의
     formData.append('productOptionId', String(productOptionId));
     formData.append('productOptionName', productOptionName);
+    // ------------------
     formData.append('rating', rating);
     formData.append('content', content);
-    formData.append('reviewImages', file);
+    files.forEach((file) => {
+      formData.append(`reviewImages`, file);
+    });
     reviewMutate({
       productId: product.id,
       formData,
@@ -102,8 +109,14 @@ export default function ReviewForm({ orderId, product, productOptionId, productO
         <button onClick={triggerFileInput} className="text-black px-4 h-[40px] rounded cursor-pointer border">
           📤 파일 선택
         </button>
-        <input type="file" accept="image/*" hidden ref={fileInputRef} onChange={handleFileChange} />
-        {preview && <img src={preview} alt="Preview" className="w-40 h-40 object-cover" />}
+        <input type="file" multiple accept="image/*" hidden ref={fileInputRef} onChange={handleFileChange} />
+        {previews.length > 0 && (
+          <div className="flex gap-2 mt-2">
+            {previews.map((preview, index) => (
+              <img key={index} src={preview} alt={`Preview ${index}`} className="w-20 h-20 object-cover" />
+            ))}
+          </div>
+        )}
       </article>
       <article className="w-full flex gap-8 py-[15px]">
         <label className="text-sm font-bold">상세 리뷰</label>
