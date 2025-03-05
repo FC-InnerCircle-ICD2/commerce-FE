b196f574ff18a61f47fde0a727756d04c605a8ec
SanDeul <tksemf1706@naver.com>
2025-03-05 09:26:38 +0900

    feat: 주문내역 페이지 재구매 버튼 기능 추가

--- src/app/order-detail/_components/OrderItem.tsx
+++ src/app/order-detail/_components/OrderItem.tsx
@@ -56,6 +56,10 @@ export default function OrderItem(props: Props) {
     router.push(`/review?orderId=${orderId}&productId=${orderProductList[0].productId}&reviewData=${queryString}`);
   };
 
+  const handleRepurchase = (product: OrderProduct) => {
+    router.push(`/products/${product.productId}`);
+  };
+
   return (
     <div className="border border-slate-300 bg-slate-50 w-full h-auto p-6 lg:p-8 flex flex-col mt-2">
       <p className="font-semibold">{orderInfo.orderStatus}</p>
@@ -88,7 +92,10 @@ export default function OrderItem(props: Props) {
             >
               리뷰쓰기
             </button>
-            <button className="border border-neutral-300 px-3.5 py-2.5 bg-white rounded-lg text-sm w-full">
+            <button
+              onClick={() => handleRepurchase(product)}
+              className="border border-neutral-300 px-3.5 py-2.5 bg-white rounded-lg text-sm w-full"
+            >
               재구매
             </button>
           </div>
